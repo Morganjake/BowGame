ac6c606b3517f858a89144d4d7a74a0c45a97ef3
MorganJake <Morgan.jake888@gmail.com>
2025-03-04 14:57:26 +0000

    Initial commit

---
+++ src/main/java/game/bow/bowgame/BowGame.java
@@ -0,0 +1,17 @@
+package game.bow.bowgame;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class BowGame extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
